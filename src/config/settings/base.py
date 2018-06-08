a77b28f5f33ac00dfe01ec499d0ea8b583705f57
zakoylov <zakoylov@gmail.com>
2018-06-08 20:56:26 +0300

    templates

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notes',
     'accounts',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-#from django.views.generic import RedirectView
+from django.views.generic import RedirectView
 from django.urls import include, path
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='notes/'), name='index'),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('notes/', include('notes.urls', namespace='notes')),
     path('admin/', admin.site.urls),
 ]
